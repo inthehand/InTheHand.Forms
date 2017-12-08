5af0fb27e24b0c9c8012652775cea0faca004a33
Peter Foot <peter@peterfoot.net>
2017-12-08 23:16:55 +0000

    AutoViewCell and sample

---
+++ InTheHand.Forms/ChatList/ChatList/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ChatList
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            this.BindingContext = new List<string> { "First message", "Second message, much longer than the first and will require significantly more space", "Third message, fairly short", "Forth message, didn't really think this through so just making up text to fill up a lot of space just to illustrate different item heights" };
+		}
+	}
+}
---
+++ InTheHand.Forms/InTheHand.Forms.Platform.iOS/AutoViewCellRenderer.cs
@@ -0,0 +1,34 @@
+﻿
+using InTheHand.Forms;
+using InTheHand.Forms.Platform.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+
+[assembly:ExportRenderer(typeof(AutoViewCell), typeof(AutoViewCellRenderer))]
+namespace InTheHand.Forms.Platform.iOS
+{
+    public sealed class AutoViewCellRenderer : ViewCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            ViewCell vc = item as ViewCell;
+
+            if (vc != null)
+            {
+                var sr = vc.View.Measure(tv.Frame.Width, double.PositiveInfinity, MeasureFlags.IncludeMargins);
+
+                if (vc.Height != sr.Request.Height)
+                {
+                    vc.ForceUpdateSize();
+
+                    sr = vc.View.Measure(tv.Frame.Width, double.PositiveInfinity, MeasureFlags.IncludeMargins);
+                    vc.Height = sr.Request.Height;
+                }
+            }
+
+            return base.GetCell(item, reusableCell, tv);
+        }
+    }
+}
