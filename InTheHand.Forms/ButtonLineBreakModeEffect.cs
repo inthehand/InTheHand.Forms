4ac6db2108ec54d4210e35bf0a42f4db5cd69b6c
Peter Foot <peter@peterfoot.net>
2018-06-15 10:43:41 +0100

    linebreakmode as an effect

---
+++ InTheHand.Forms/ButtonLineBreakModeEffect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace InTheHand.Forms
+{
+    public sealed class ButtonLineBreakModeEffect : RoutingEffect
+    {
+        /// <summary>
+        /// Gets or sets the LineBreakMode for the Button
+        /// </summary>
+        public LineBreakMode LineBreakMode { get; set; }
+
+        public ButtonLineBreakModeEffect() : base(typeof(ButtonLineBreakModeEffect).FullName) { }
+    }
+}
